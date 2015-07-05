2978e8fdaf4fcc31e1358ff0f91b2d41d0052fd7
cwjpalmer <cwjpalmer@gmail.com>
2015-07-05 18:00:01 +0200

    basic library framework

---
+++ buildsterbot/buildsterbot.cpp
@@ -0,0 +1,8 @@
+/*
+  buildsterbot library
+  copyright Christopher Walter James Palmer 2015
+*/
+
+buildsterbot::buildsterbot() {
+  
+}
---
+++ buildsterbot/buildsterbot.h
@@ -0,0 +1,20 @@
+/*
+  buildsterbot library
+  copyright Christopher Walter James Palmer 2015
+*/
+
+
+
+#ifndef buildsterbot_h
+#define buildsterbot_h
+
+#include "Arduino.h"
+
+class buildsterbot {
+  public:
+    buildsterbot();
+  private:
+};
+
+
+#endif
